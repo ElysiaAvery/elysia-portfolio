1b258b8946847f8170cffbf85c90b75f8dfc8d7b
Elysia Nason <elysia.avery@gmail.com>
2017-03-11 20:10:58 -0800

    Add About page with styling for all screen sizes

--- app/app.php
+++ app/app.php
@@ -16,6 +16,19 @@
     return $app['twig']->render('index.html.twig');
   });
 
+  $app->get("/about", function() use ($app) {
+    $description = 'I am an ambitious and motivated Developer. I began coding when I was a teen ( and it was only basic HTML! ). I also love Illustrating and feel that shows in the design of my applications. My current favorite languages to write in are JavaScript, Java, and PHP.';
+    return $app['twig']->render('about.html.twig', array('medescription' => $description));
+  });
+
+  $app->get("/contact", function() use ($app) {
+    return $app['twig']->render('contact.html.twig');
+  });
+
+  $app->get("/volunteer", function() use ($app) {
+    return $app['twig']->render('volunteer.html.twig');
+  });
+
 
   return $app;
 ?>
